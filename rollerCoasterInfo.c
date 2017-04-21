390c28f843d0453260806664ce7bc9d1fc4fe9bc
Ana Requena <anachan.barbosa@gmail.com>
2017-04-20 23:38:11 -0300

    Implementacao em C
    Coloquei os semaforos do jeito que a prof colocou nos slides, fiz uma base mais concreta das threads passenger e car e iniciei a main. Por enquanto só tem um carrinho pq não sei fazer com mais :c

--- rollerCoasterInfo.c
+++ rollerCoasterInfo.c
@@ -20,14 +20,24 @@ Write code for the passengers and car that enforces these constraints.*/
 
 /*5.8.1 Roller Coaster hint*/
 
-mutex = Semaphore (1);
-mutex2 = Semaphore (1);
-boarders = 0;
-unboarders = 0;
-boardQueue = Semaphore (0);
-unboardQueue = Semaphore (0);
-allAboard = Semaphore (0);
-allAshore = Semaphore (0);
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ncurses.h>
+
+#define N_PASSENGERS 8;
+
+sem_t mutex;
+sem_t mutex2;
+sem_t boardQueue;
+sem_t unboardQueue;
+sem_t allAboard;
+sem_t allAshore;
+
+int boarders = 0;
+int unboarders = 0;
 
 /*mutex protects passengers, which counts the number of passengers that have
 invoked boardCar. Passengers wait on boardQueue before boarding and unboardQueue
@@ -39,47 +49,89 @@ Here is my code for the car thread:
 
 Roller Coaster solution (car) */
 
-load ();
-boardQueue.signal ( C );
-allAboard.wait ();
+void* carThread(void* v){
+	load(); /* enche o carrinho com C passageiros */
+	
+	sem_post(&boardQueue);
+	sem_wait(&allAboard);
+//	boardQueue.signal ( C );
+//	allAboard.wait ();
 
-run ();
+	run(); /* coloca carrinho em movimento */
 
-unload ();
-unboardQueue.signal ( C );
-allAshore.wait ();
-
+	unload (); /* desembarque de passageiros */
+	
+	sem_post(&unboardQueue);
+	sem_post(&allAshore);
+//	unboardQueue.signal ( C );
+//	allAshore.wait ();
+}
 /*When the car arrives, it signals C passengers, then waits for the last one to
 signal allAboard. After it departs, it allows C passengers to disembark,
 then waits for allAshore.
 
 Roller Coaster solution (passenger) */
+void* passengerThread(void* v){
+	sem_wait(&boardQueue);
+//	boardQueue.wait ();
+	board (); /* embarca no carrinho */
 
-boardQueue.wait ();
-board ();
+	sem_wait(&mutex);
+//	mutex.wait ();
+	boarders += 1;
+	
+	if (boarders == C){
+		sem_post(&allAboard); /* indica que tem C passageiros a bordo */ 
+//	  	allAboard . signal ();
+	  	boarders = 0;
+	}
+	
+	sem_post(&mutex);
+//	mutex.signal ();
 
-mutex.wait ();
-boarders += 1;
-if (boarders == C){
-  allAboard . signal ();
-  boarders = 0;
+	sem_wait(&unboardQueue);
+//	unboardQueue.wait ();
+	unboard (); /* desembarca passageiro */
+
+	sem_wait(&mutex2);
+//	mutex2.wait ();
+	unboarders += 1;
+	if (unboarders == C){
+		sem_post(&allAshore);
+//	  	allAshore.signal ();
+		unboarders = 0;
+	}
+	
+	sem_post(&mutex2);
+//	mutex2.signal ();
 }
-mutex.signal ();
-
-unboardQueue.wait ();
-unboard ();
-
-mutex2.wait ();
-unboarders += 1;
-if (unboarders == C){
-  allAshore.signal ();
-  unboarders = 0;
-}
-mutex2.signal ();
-
 /*Passengers wait for the car before boarding, naturally, and wait for the car to
 stop before leaving. The last passenger to board signals the car and resets the
 passenger counter.*/
+
+int main(){
+	pthread_t passengers[N_PASSENGERS], car;
+	int i, id[N_PASSENGERS];
+	
+	sem_init(&mutex, 0, 1);
+	sem_init(&mutex2, 0, 1);
+	sem_init(&boardQueue, 0, 4); // 4 passageiros num carrinho
+	sem_init(&unboardQueue, 0, 4);
+	sem_init(&allAboard, 0, 1);
+	sem_init(&allAshore, 0, 1);
+	
+	for(i = 0; i < N_PASSENGERS; i++){
+		id[i] = i;
+		pthread_create(&passengers[i], NULL, passengerThread, (void*) &id[i]);
+	}
+	
+	pthread_create(&car, NULL, carThread, NULL);
+	
+	for(i = 0; i < N_PASSENGERS; i++)
+		pthread_join(passenger[i], NULL);
+	
+	return 0;
+}
 
 /*============================================================================*/
 
